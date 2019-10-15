f688f8bbc05ded7687a1ce2b5768f8447e97227f
Keith <kthompson.mcs@gmail.com>
2019-10-15 13:59:02 -0500

    added bash scripts

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+HOST = "127.0.0.1"
+PORT = "6666"
+
+./server.exe $PORT
+echo
+echo "Test 1"
+./client.exe $HOST $PORT < test1.txt
+echo
+echo "Test 2"
+./client.exe $HOST $PORT < test2.txt
+echo
+echo "Test 3"
+./client.exe $HOST $PORT < test3.txt
+echo
+echo "Test 4"
+./client.exe $HOST $PORT < test4.txt
+echo
