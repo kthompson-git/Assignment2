a3fca709e4cebc545510e43e36a4bd7dba230544
Keith <kthompson.mcs@gmail.com>
2019-10-15 14:14:57 -0500

    added test files

--- server.cpp
+++ server.cpp
@@ -3,12 +3,12 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/types.h> 
-#include <sys/socket.h>
-#include <netinet/in.h>
+// #include <sys/socket.h>
+// #include <netinet/in.h>
 
-void error(char *msg)
+void error(std::string msg)
 {
-    perror(msg);
+    std::cout << msg;
     exit(1);
 }
 
