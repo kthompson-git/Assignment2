76ae2bcbdcdce4827b97b387e24fcd046f74f5ec
Keith <kthompson.mcs@gmail.com>
2019-10-15 14:17:31 -0500

    include libraries

--- server.cpp
+++ server.cpp
@@ -3,8 +3,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/types.h> 
-// #include <sys/socket.h>
-// #include <netinet/in.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 void error(std::string msg)
 {
